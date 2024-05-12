95d752ca41dbaba6bbc91350184a82c49d1ff8ff
Ayush Sahrawat <ayushsahrawat47@gmail.com>
2024-05-12 21:08:15 +0530

    log in functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-const JWT_SECRET ='Ayushisagood$oy'
+const JWT_SECRET = 'Ayushisagood$oy'
 
 
 //creating a new user using POST "/api/auth/createuser"
@@ -37,17 +37,55 @@ router.post('/createuser', [
                 password: secPass,
                 email: req.body.email,
             });
-            const data ={
-                user:{
-                    id:user.id
+            const data = {
+                user: {
+                    id: user.id
                 }
             }
             const authToken = jwt.sign(data, JWT_SECRET);
             res.json(authToken)
-        } catch(error){
-            console.log(error.message);
-            res.status(500).send("Internal Server Error")   
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error")
         }
+    })
+
+
+// Authenticate a user using : POST "/api/auth/login"
+router.post('/login', [
+    body('email', 'naa badhwae koi account nahi hai tera account bana').isEmail(),
+    body('password', 'phir backchodi de raha hai').exists(),
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+        try{
+            let user = await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error: "wrong credentials"})
+            }
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if(!passwordCompare){
+                return res.status(400).json({error: "wrong credentials"})
+            }
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+            res.send("Logged In successfully!!")
+        }
+        catch(error){
+            console.error(error.message);
+            res.status(500).send("Internal Server Error")
+        }
+
+        
     })
 
 module.exports = router
